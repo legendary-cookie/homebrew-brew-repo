0f02348f5cce0c36695f3ebd875334de8e3b8955
Cook1e <vincent.schweiger@xolley.de>
2021-06-02 12:59:23 +0200

    pongoterm

--- Formula/pongoterm.rb
+++ Formula/pongoterm.rb
@@ -1,5 +1,5 @@
 class Pongoterm < Formula
-  desc "pongoterm"
+  desc "Connect to iOS device that has PongoOS loaded"
   homepage "https://checkra.in"
   url "https://github.com/legendary-cookie/pongoOS/archive/refs/tags/pongoterm-1.tar.gz"
   sha256 "0de2d02e02e88c2e6e7f1a78ec9f06d5843f55c8dd54aff3abcf4f637469909b"
