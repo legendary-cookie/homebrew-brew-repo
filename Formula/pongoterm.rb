c04cddc192a0cedc5df918bc7ed0f1d4679142c7
Cook1e <vincent.schweiger@xolley.de>
2021-06-02 12:55:22 +0200

    added pongoterm

---
+++ Formula/pongoterm.rb
@@ -0,0 +1,17 @@
+class Pongoterm < Formula
+  desc "pongoterm"
+  homepage "https://checkra.in"
+  url "https://github.com/legendary-cookie/pongoOS/archive/refs/tags/pongoterm-1.tar.gz"
+  sha256 "0de2d02e02e88c2e6e7f1a78ec9f06d5843f55c8dd54aff3abcf4f637469909b"
+  license "MIT"
+
+  def install
+   system "cd scripts && make pongoterm"
+   bin.mkpath
+   bin.install "scripts/pongoterm"
+  end
+
+  test do
+    system "false"
+  end
+end
